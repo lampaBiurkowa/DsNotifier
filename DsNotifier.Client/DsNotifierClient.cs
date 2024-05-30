0c1034c386b3149abd0e119acd70e93dcb34155e
hnoj <mrocznaklawiatura@gmail.com>
2024-05-30 15:52:00 +0200

    send heneric :|

--- DsNotifier.Client/DsNotifierClient.cs
+++ DsNotifier.Client/DsNotifierClient.cs
@@ -6,9 +6,11 @@ namespace DsNotifier.Client;
 public interface IDsNotifierClient
 {
     Task SendDsLauncherPurchasedEvent(PurchasedEvent e, CancellationToken ct);
+    Task SendGeneric<T>(T e, CancellationToken ct) where T : class;
 }
 
 class DsNotifierMassTransitClient(IPublishEndpoint publishEndpoint) : IDsNotifierClient
 {
     public async Task SendDsLauncherPurchasedEvent(PurchasedEvent e, CancellationToken ct) => await publishEndpoint.Publish(e, ct);
+    public async Task SendGeneric<T>(T e, CancellationToken ct) where T : class => await publishEndpoint.Publish(e, typeof(T), ct);
 }
